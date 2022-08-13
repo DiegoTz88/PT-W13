da8d5d1445d4afce4831535807e8bb89eb6782bf
Diego Tzompa <tzompad@gmail.com>
2022-08-12 20:19:30 -0600

    Second commit

--- src/main/java/com/promineotech/jeep/dao/JeepSalesDao.java
+++ src/main/java/com/promineotech/jeep/dao/JeepSalesDao.java
@@ -1,11 +1,11 @@
-package com.promineotech.jeep.service;
+package com.promineotech.jeep.dao;
 
 import java.util.List;
 
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
-public interface JeepSalesService {
+public interface JeepSalesDao {
 
 	/**
 	 * 
@@ -15,7 +15,5 @@ public interface JeepSalesService {
 	 */
 	
 	List<Jeep> fetchJeeps(JeepModel model, String trim);
-
-
 
 }
--- src/main/java/com/promineotech/jeep/entity/Jeep.java
+++ src/main/java/com/promineotech/jeep/entity/Jeep.java
@@ -1,6 +1,10 @@
 package com.promineotech.jeep.entity;
 
 import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.Comparator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,12 +14,29 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Jeep {
+public class Jeep implements Comparable<Jeep>{
   private Long modelPK;
   private JeepModel modelId;
   private String trimLevel;
   private int numDoors;
   private int wheelSize;
   private BigDecimal basePrice;
+  
+  @JsonIgnore
+  public Long getModelPk() {
+	  return modelPK;
+  }
+
+@Override
+public int compareTo(Jeep that) {
+	// TODO Auto-generated method stub
+	//@formatter:off
+	return Comparator
+		.comparing(Jeep::getModelId)
+		.thenComparing(Jeep::getTrimLevel)
+		.thenComparing(Jeep::getNumDoors)
+		.compare(this, that);
+	//@formatter:on
+}
 }
 
